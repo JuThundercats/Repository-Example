83ae105f96d220ed1422be5a5cc45052599c6391
[Thundercats] <engenhariasistemas02@gmail.com>
2012-09-20 22:31:51 -0300

    suceed

--- file.c
+++ file.c
@@ -1,24 +1,33 @@
+#include <stdio.h>
 
-
-j=0;
-
-for(i=1;i<=n/2;i++)
+int main ()
 {
-  if(n%i==0))
-    divisores[j++]=i;
-}
-x=0;
+        int j,i,divisores[10000],x,n;
 
-for(i=0;i<=j;i++)
-{
-    x+=divisores[i];
-}
+        scanf ("%d",&n);
 
-if (x==n)
-{
-  printf("%d is a perfect number",n);
-}
-else
-{
-  printf("%d is not a perfect number",n);
+        j=0;
+
+        for(i=1;i<=n/2;i++)
+        {
+          if(n%i==0)
+            divisores[j++]=i;
+        }
+        x=0;
+
+        for(i=0;i<=j;i++)
+        {
+            x+=divisores[i];
+        }
+
+        if (x==n)
+        {
+          printf("%d is a perfect number",n);
+        }
+        else
+        {
+          printf("%d is not a perfect number",n);
+        }
+    return 0;
+
 }
